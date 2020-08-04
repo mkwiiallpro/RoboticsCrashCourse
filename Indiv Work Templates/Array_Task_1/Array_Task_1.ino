f18f67394ee7f94783c0676fbf2d32974eae8f7f
mkwiiallpro <mkwiiallpro@gmail.com>
2020-08-04 10:59:53 -0400

    Added array task 1

--- Indiv Work Templates/Array_Task_1/Array_Task_1.ino
+++ Indiv Work Templates/Array_Task_1/Array_Task_1.ino
@@ -4,13 +4,17 @@ void setup() {
   Serial.begin(9600);
 
   // Creat an array of type int and size 10:
-
+  int ten[10];
   // Using a while loop, fill the array with multiples of 3 between 3 and 30 (inclusive):
                 // Remember the syntax?
                 // while (condition) {
                 //       code...
                 // }
-
+  int x=0;
+  while(x<10){
+    ten[x]= 3*x+3;
+    x++;
+  }
 
 
   // Rewrite the code above with a for loop:
@@ -20,11 +24,14 @@ void setup() {
                 // }
 
 
-
   // Write a loop that will go through the array 
   //     and print any values that are a multiple of 9:
                 // Hint: Use the % (modulus) operator.
-
+  for(int i =0;i<10;i++){
+    if(ten[i]%9 == 0){
+      Serial.println(ten[i]);
+    }
+  }
 
 }
 
